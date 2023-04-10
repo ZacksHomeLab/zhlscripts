bf5582c632963f70ade09d7f95aef229e8a17812
zack@zackshomelab.com <zack@zackshomelab.com>
2023-04-10 02:48:54 -0500

    Finished New-LECertificate.ps1

--- scripts/misc/New-LECertificate.ps1
+++ scripts/misc/New-LECertificate.ps1
@@ -12,35 +12,40 @@
     Comma separated FQDN list of additional SANs for your SSL Certificate.
 .PARAMETER CFToken
     The CloudFlare token to authorize said API calls.
-.PARAMETER CFEmail
-    If your CloudFlare Email Address is different than $Email, provide it here.
-.PARAMETER AzCreds
-    The credentials used to connect to Azure.
+.PARAMETER CFFile
+    The path of the file that will store your DNS Plugin Credentials.
 .PARAMETER KeyVaultName
     The name of your Key Vault in Azure.
 .PARAMETER KeyVaultItem
     The name of the key vault item that holds your CloudFlare API Token
-.PARAMETER CreateScheduleTask
-    Pass this switch if you would like to create a scheduled task on your computer to renew the SSL Certificate.
-.PARAMETER CreateCronTask
-    Pass this switch if you would like to create a Cron job on your machine to renew the SSL Certificate.
-.PARAMETER RenewSSL
-    Pass this parameter if you are renewing the certificate rather than creating a new one.
+.PARAMETER KeyVaultItemVersion
+    The specific version of your Key Vault Item to use.
+.PARAMETER AzCreds
+    The credentials used to connect to Azure.
+.PARAMETER DNSPlugin
+    The DNS Plugin certbot will use.
+.PARAMETER DryRun
+    Pass this switch to perform a test run without creating any certificates. 
+.PARAMETER CreateRenewalTask
+    Pass this switch if you would like to create a scheduled task on your machine to renew said SSL Certificate.
 .EXAMPLE
-    ./New-ZHLAzAutomationAccount.ps1 -SubscriptionName "ZacksHomeLab" -Name "ZHLAutoAccount" -ResourceGroupName "ZHL" -AzCreds (Get-Credential) -tags @{'zhl-resource'='zhl-automation-account'}
+    ./New-LECertificate.ps1 -Email 'zack@zackshomelab.com' -Hostname 'test.zackshomelab.com' -SANs 'test2.zackshomelab.com', 'test3.zackshomelab.com' `
+        -CFToken (Read-Host -Prompt "Enter your CF Token" -AsSecureString) -CFFile "$($ENV:HOME)/.secrets/le_creds.json" -DNSPlugin CloudFlare `
+        -CreateRenewalTask
 
-    The above will create an Automation Account named 'ZHLAutoAccount' within Resource Group 'ZHL' and apply tags 'zhl-resource:zhl-automation-account' onto said account.
-.EXAMPLE
-    ./New-ZHLAzAutomationAccount.ps1 -SubscriptionName "ZacksHomeLab" -Name "ZHLAutoAccount" -ResourceGroupName "ZHL" -AzCreds (Get-Credential) -tags @{'zhl-resource'='zhl-automation-account'} -DisablePublicNetworkAccess
-    
-    The above will create an Automation Account named 'ZHLAutoAccount' within Resource Group 'ZHL' and apply tags 'zhl-resource:zhl-automation-account' onto said account.
-    Once created, we'll update the Automation Account to disable public access.
+    The above performs the following:
+      * Creates a SSL Certificate with the hostname of test.zackshomelab.com
+      * Adds two additional domains ('test2 & test3) to the certificate
+      * Asks the user to input their CloudFlare Token for authorization
+      * Uses Email 'zack@zackshomelab.com' as the notification email
+      * Store the token at path $($ENV:HOME)/.secrets/le_creds.json (translates to /root/.secrets/le_creds.json)
+      * Creates a scheduled task (cron job in my example as I'm running this on Linux) to renew said certificate
 .NOTES
     Author - Zack
 .LINK
     GitHub - https://github.com/ZacksHomeLab
 #>
-[cmdletbinding()]
+[cmdletbinding(DefaultParameterSetName="default")]
 param (
     [parameter(Mandatory,
         ValueFromPipelineByPropertyName,
@@ -66,13 +71,11 @@ param (
         ValueFromPipelineByPropertyName,
         ParameterSetName="CF",
         Position=3)]
-    [string]$CFEmail,
+    [System.Security.SecureString]$CFToken,
 
     [parameter(Mandatory,
-        ValueFromPipelineByPropertyName,
-        ParameterSetName="CF",
-        Position=4)]
-    [System.Security.SecureString]$CFToken,
+        ValueFromPipelineByPropertyName)]
+    [String]$CFFile,
 
     [parameter(Mandatory,
         ValueFromPipelineByPropertyName,
@@ -83,8 +86,7 @@ param (
 
     [parameter(Mandatory,
         ValueFromPipelineByPropertyName,
-        ParameterSetName="KeyVault",
-        Position=4)]
+        ParameterSetName="KeyVault")]
         [ValidateNotNullOrEmpty()]
     [string]$KeyVaultItem,
 
@@ -95,19 +97,28 @@ param (
     [string]$KeyVaultItemVersion,
 
     [parameter(Mandatory=$false,
-        ValueFromPipelineByPropertyName)]
-        [ValidateSet("CloudFlare")]
-    [string]$DNSPlugin,
-
-    [parameter(Mandatory=$false,
         ValueFromPipelineByPropertyName,
         ParameterSetName="KeyVault")]
         [ValidateNotNullorEmpty()]
-    [System.Management.Automation.PSCredential]$AzCreds
+    [System.Management.Automation.PSCredential]$AzCreds,
+
+    [parameter(Mandatory=$false,
+        ValueFromPipelineByPropertyName)]
+        [ValidateSet("CloudFlare")]
+    [string]$DNSPlugin = "CloudFlare",
+
+    [switch]$DryRun,
+    [switch]$CreateRenewalTask
 )
 
 #region Variables
-$leParams = @{}
+$leParams = @()
+$leCommand = $null
+$snapExists = $false
+$pipExists = $false
+$secret = $null
+$existingToken = $null
+$crontabFile = $null
 #endregion
 
 #region Functions
@@ -172,14 +183,83 @@ function Import-AZ {
         }
     }
 }
+
+function Start-Command {
+    [cmdletbinding()]
+    param (
+        [parameter(Mandatory,
+            Position=0,
+            ValueFromPipelineByPropertyName)]
+            [ValidateScript({Test-Path -Path $_})]
+        [string]$CommandPath,
+
+        [parameter(Mandatory=$false,
+            Position=1,
+            ValueFromPipelineByPropertyName)]
+            [ValidateNotNullOrEmpty()]
+        [string]$CommandArguments,
+
+        [parameter(Mandatory=$false,
+            Position=2)]
+            [ValidateNotNullOrEmpty()]
+        [string]$CommandTitle = "Command"
+    )
+
+    begin {
+        $commandInfo = New-Object System.Diagnostics.ProcessStartInfo
+        $process = New-Object System.Diagnostics.Process
+    }
+    process {
+
+        try {
+            $commandInfo.FileName = $commandPath
+            $commandInfo.RedirectStandardError = $true
+            $commandInfo.RedirectStandardOutput = $true
+            $commandInfo.UseShellExecute = $false
+            if ($PSBoundParameters.ContainsKey('commandArguments')) {
+                $commandInfo.Arguments = $commandArguments
+            }
+            
+            $process.StartInfo = $commandInfo
+            $process.Start() | Out-Null
+            $process.WaitForExit()
+            [pscustomobject]@{
+                Title = $commandTitle
+                Output = $process.StandardOutput.ReadToEnd()
+                Error = $process.StandardError.ReadToEnd()
+                ExitCode = $process.ExitCode
+            }
+        } catch {
+            Throw "Start-Command: Failed running command $commandPath due to error $_"
+        }
+    }
+}
 #endregion
 
 #region ExitCodes
+$exitcode_NotRoot = 9
 $exitCode_MissingPowerShellModule = 10
 $exitCode_ErrorConnectingToAzure = 11
 $exitCode_FailureRetrievingSecret = 12
 $exitcode_MissingCertbot = 13
-$exitCode_MissingSnap = 14
+$exitCode_MissingSnapAndPip = 14
+$exitCode_FailCreateCredsFile = 15
+$exitCode_MissingPIPDNSPlugin = 16
+$exitCode_MissingSnapDNSPlugin = 17
+$exitCode_FailureCreateCert = 18
+$exitCode_FailureCreateCrontabFile = 19
+$exitCode_FailureCreateCronJob = 20
+#endregion
+
+#Requires -RunAsAdministrator
+
+#region Check if Admin
+if ([System.Environment]::OSVersion.Platform -eq "Unix") {
+    if ($(whoami) -ne "root") {
+        Write-Warning "Main: You must run this script as root, stopping."
+        exit $exitcode_NotRoot
+    }
+}
 #endregion
 
 #region Verify Certbot exists
@@ -191,33 +271,62 @@ if ($null -eq (Get-Command -Name "certbot")) {
 
 #region verify our DNS Plugin exists
 if ($PSBoundParameters.ContainsKey('DNSPlugin')) {
-    if ($null -eq (Get-Command -Name "snap" -ErrorAction SilentlyContinue)) {
-        Write-Warning "Snap is not installed, stopping."
-        exit $exitCode_MissingSnap
+    $pipPlugin = $null
+    $snapPlugin = $null
+
+    if ($null -ne (Get-Command -Name "pip" -ErrorAction SilentlyContinue)) {
+        $pipExists = $true
+    } 
+    if ($null -ne (Get-Command -Name "snap" -ErrorAction SilentlyContinue)) {
+        $snapExists = $true
+    } 
+    if ($snapExists -eq $false -and $pipExists -eq $false) {
+        Write-Warning "You need to have your DNS Plugin installed via 'snap' or 'pip' before proceeding."
+        exit $exitCode_MissingSnapAndPip
     }
 
-    
+    # Check if Plugin exists
+    if ($pipExists) {
+        $pipPlugin = (Start-Command -CommandPath (Get-Command -Name 'pip').Source -CommandArguments "list" -CommandTitle "pip").Output
+        
+        if (-not ($pipPlugin -match "certbot-dns-$($DNSPlugin.ToLower())")) {
+            if (-not $snapExists) {
+                Write-Warning "Missing Certbot DNS Resolver plugin: 'certbot-dns-$($DNSPlugin.toLower())'. Run 'pip install certbot-dns-$($DNSPlugin.toLower())'"
+                exit $exitCode_MissingPIPDNSPlugin
+            } else {
+                Write-Warning "Missing Certbot DNS Resolver plugin in 'pip'. Going to check if it exists via 'snap'..."
+            }
+            
+        }
+    }
+    if ($snapExists) {
+        $snapPlugin = Start-Command -CommandPath (Get-Command -Name 'snap').Source -CommandArguments "install certbot-dns-$($DNSPlugin.toLower())" -CommandTitle "snap"
+        if ($snapPlugin.ExitCode -ne 0) {
+            Write-Warning "Missing Certbot DNS Resolver plugin: 'certbot-dns-$($DNSPlugin.toLower())'. Run 'snap install certbot-dns-$($DNSPlugin.toLower())'"
+            exit $exitCode_MissingSnapDNSPlugin
+        }
+    }
 }
 #endregion
 
-#region Connect to Azure Application
-try {
-    Write-Verbose "Attempting to import Module AZ.Accounts..."
-    if (Test-Path -Path "C:\Program Files\WindowsPowerShell\Modules\Az.Accounts") {
-        Import-Module 'C:\Program Files\WindowsPowerShell\Modules\Az.Accounts' -ErrorAction SilentlyContinue
-    } else {
-        Import-AZ -Module 'AZ.KeyVault', "AZ.Accounts" -ErrorAction Stop
-    }
-} catch {
-    Write-Warning "Failed to Import Module 'AZ.Accounts' and 'AZ.KeyVault' due to error $_."
-    exit $exitCode_MissingPowerShellModule
-}
-
-#region Connect to Azure
+#region Connect to Azure and retrieve KeyVaultItem
 if ($PSCmdlet.ParameterSetName -eq 'KeyVault') {
 
     $connectParams = @{}
     $secretParams = @{}
+
+    try {
+        Write-Verbose "Attempting to import Module AZ.Accounts..."
+        if (Test-Path -Path "C:\Program Files\WindowsPowerShell\Modules\Az.Accounts") {
+            Import-Module 'C:\Program Files\WindowsPowerShell\Modules\Az.Accounts' -ErrorAction SilentlyContinue
+        } else {
+            Import-AZ -Module 'AZ.KeyVault', "AZ.Accounts" -ErrorAction Stop
+        }
+    } catch {
+        Write-Warning "Failed to Import Module 'AZ.Accounts' and 'AZ.KeyVault' due to error $_."
+        exit $exitCode_MissingPowerShellModule
+    }
+
     try {
         # Add AZ Creds if provided
         if ($PSBoundParameters.ContainsKey('AzCreds')) {
@@ -247,5 +356,145 @@ if ($PSCmdlet.ParameterSetName -eq 'KeyVault') {
         exit $exitCode_FailureRetrievingSecret
     }
     
+}
+#endregion
+
+#region Store credentials for later usage if the token were provided
+if ([System.Environment]::OSVersion.Platform -eq "Unix") {
+
+    if ($PSCmdlet.ParameterSetName -eq 'CF') {
+        $secret = $CFToken
+    } elseif ($PSCmdlet.ParameterSetName -eq 'KeyVault') {
+        $secret = $KeyVaultSecret
+    }
+   
+    if (-not (Test-Path -Path $CFFile)) {
+        if (-not (Test-Path -Path $(Split-Path -Path $CFFile -Parent))) {
+            Write-Verbose "Creating directory $(Split-Path -Path $CFFile -Parent)..."
+            New-Item -Path $(Split-Path -Path $CFFile -Parent) -ItemType Directory -Force | Out-Null
+        }
+
+        # Save token to $CFFile if $CFToken was provided
+        try {
+            Write-Verbose "Saving Secret to $CFFile..."
+            Tee-Object -FilePath $CFFile -InputObject "dns_cloudflare_api_token=$($secret | ConvertFrom-SecureString -AsPlainText)" -ErrorAction Stop | Out-Null
+        } catch {
+            Write-Warning "Failure creating credential file at '$CFFile' due to error $_"
+            exit $exitCode_FailCreateCredsFile
+        }
+        
+        # Modify file permissions for le_creds.json
+        Write-Verbose "Changing ownership and permissions for $CFFile.."
+        Start-Command -CommandPath (Get-Command -Name 'chown').Source -CommandArguments "root:root $CFFile" -CommandTitle "chown"
+        Start-Command -CommandPath (Get-Command -Name 'chmod').Source -CommandArguments "600 $CFFile" -CommandTitle "chown"
+
+    } else {
+
+        # Tokenfile exists, retrieve content
+
+        Write-Verbose "Retrieving token from $CFFile (if it exists)"
+        $existingToken = $((Select-String -Path $CFFile -Pattern '(?<=\=\s*)(.*)' -AllMatches -ErrorAction SilentlyContinue).Matches.Value.Trim()) | ConvertTo-SecureString -AsPlainText -ErrorAction SilentlyContinue
+
+        # if existing token is null and user didn't provide a token, end script.
+        if ($PSCmdlet.ParameterSetName -eq "Default" -and ($null -eq $existingToken -or $existingToken -eq "")) {
+            Write-Warning "Secret file $CFFile is empty and you did not provide a CFToken nor KeyVaultItem, stopping."
+            exit $exitCode_FailCreateCredsFile
+        }
+
+        # Check if the provided secret matches the existing secret
+        if (($secret | ConvertFrom-SecureString -AsPlainText) -ne ($existingToken | ConvertFrom-SecureString -AsPlainText)) {
+            try {
+                Write-Verbose "The provided token is different than the saved token, overwriting..."
+                Tee-Object -FilePath $CFFile -InputObject "dns_cloudflare_api_token=$($secret | ConvertFrom-SecureString -AsPlainText)" -ErrorAction Stop | Out-Null
+            } catch {
+                Write-Warning "Failure creating credential file at '$CFFile' due to error $_"
+                exit $exitCode_FailCreateCredsFile
+            }
+        }
+    }
+} else {
+    # Windows Stuff here
+}
+#endregion
+
+#region Create SSL Certificate
+try {
+    #region Build Certbot command
+    $leParams += "certonly"
+    if ($PSBoundParameters.ContainsKey('DNSPlugin')) {
+        $leParams += "--dns-$($DNSPlugin.toLower())"
+        $leParams += "--dns-$($DNSPlugin.toLower())-propagation-seconds 30"
+        $leParams += "--dns-$($DNSPlugin.toLower())-credentials $CFFile"
+    }
+
+    $leParams += "-d $($Hostname.toLower())"
+    if ($PSBoundParameters.ContainsKey('SANs')) {
+        foreach ($SAN in $SANs) {
+            if ($null -ne $SAN -and $SAN -ne "") {
+                $leParams += "-d $($SAN.toLower())"
+            }
+        }
+    }
+    $leParams += "--agree-tos"
+    # Run non-interactively
+    $leParams += "-n"
+    $leParams += "-m $Email"
+
+    if ($DryRun) {
+        $leParams += "--dry-run"
+    }
+    #endregion
+    
+    # Create the Let's Encrypt Command
+    $leCommand = $leParams -join ' '
+
+    # Run the certificate
+    Write-Verbose "Attempting to run 'certbot $leCommand..."
+    $certCommand = Start-Command -CommandPath (Get-Command -Name 'certbot').Source -CommandArguments $leCommand -CommandTitle "Certbot"
+
+    if ($certCommand.ExitCode -ne 0) {
+        Write-Warning "Certbot failed with exit code $($certCommand.ExitCode) due to error $($certCommand.Error)"
+        exit $exitCode_FailureCreateCert
+    }
+} catch {
+    Write-Warning "Failure creating certificate due to error $_"
+}
+#endregion
+
+#region Create Cron job
+if ($CreateRenewalTask -and $DryRun -ne $true) {
+    if ([System.Environment]::OSVersion.Platform -eq "Unix") {
+        $crontabFile = "/var/spool/cron/crontabs/$(whoami)"
+        Write-Verbose "Before creating the cron job, verify it doesn't exist"
+        if (Test-Path -Path $crontabFile) {
+            $crontabFileContent = $((Select-String -Path $crontabFile -Pattern "certbot renew --dns-$($DNSPlugin.toLower())-propagation-seconds 20$" `
+                -AllMatches -ErrorAction SilentlyContinue).Matches.Value.Trim())
+            if ($null -ne $crontabFileContent -and $crontabFileContent -ne "") {
+                Write-Verbose "Cron job already exists"
+                exit 0
+            }
+        } else {
+            try {
+                Write-Verbose "Crontab file doesn't exist, creating it now..."
+                New-Item -Path $crontabFile -ItemType File -ErrorAction Stop
+            } catch {
+                Write-Warning "Failure creating crontab file $crontabFile due to error $_"
+                exit $exitCode_FailureCreateCrontabFile
+            }
+        }
+
+        try {
+            Write-Verbose "Cron job doesn't exist, adding said job to file $crontabFile"
+            Add-Content -Path $crontabFile -Value "0 0,12 * * * certbot renew --dns-$($DNSPlugin.toLower())-propagation-seconds 20" -ErrorAction Stop
+        } catch {
+            Write-Warning "Failure creating cron job in file $crontabFile due to error $_"
+            exit $exitCode_FailureCreateCronJob
+        }
+    }
+} else {
+    if ($CreateRenewalTask -and $DryRun) {
+        Write-Warning "You cannot create a scheduled renewal job while passing -DryRun."
+        exit 0
+    }
 }
 #endregion
